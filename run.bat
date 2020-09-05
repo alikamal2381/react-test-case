78b1896f63d20211b53cd953051aff2329dea0c8
AliKamal <alikamal2381@gmail.com>
2020-09-05 23:17:57 +0500

    second-commit

--- run.bat
+++
@@ -1,7 +0,0 @@
-echo "# react-test-case" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/alikamal2381/react-test-case.git
-git push -u origin master
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello World/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("test case", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Panacloud bootcamp2020/i);
   expect(linkElement).toBeInTheDocument();
 });
